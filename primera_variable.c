6647b11cc841b21b09eab27b9defb856f221f53a
Isabel Yepes <63390283+isabelyb@users.noreply.github.com>
2020-09-11 16:44:37 -0700

    Add files via upload

---
+++ primera_variable.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <stdbool.h> 
+
+
+int main()
+{
+  int integerA = 1;
+  float floatA = 1.1;
+  double doubleA = 1.2;
+  char letterA = 'A';
+ 
+ //En la siguiente liinea imprimo mi variable integerA
+  printf("El valor de mi entero es: %i \n", integerA);
+
+  printf("El valor de mi flotante es: %f \n", floatA);
+
+  printf("El valor de mi bouble es: %f \n", doubleA); 
+
+  printf("El valor de mi caracter es: %c \n", letterA );
+
+  return 0;
+}
